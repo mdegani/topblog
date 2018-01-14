8e6854ebc756076d1c53fa59435423f9a5e8cb18
Mark Degani <mark.degani@gmail.com>
2018-01-14 13:01:49 -0500

    ga

--- gatsby-config.js
+++ gatsby-config.js
@@ -2,6 +2,13 @@ module.exports = {
   siteMetadata: {
     title: `Mark Degani`,
   },
-  plugins: [`gatsby-plugin-react-helmet`],
+  plugins: [`gatsby-plugin-react-helmet`,
+  {
+    resolve: `gatsby-plugin-google-analytics`,
+    options: {
+      trackingId: "UA-112465882-1"
+    },
+  },
+],
   pathPrefix: `/`,
 }
