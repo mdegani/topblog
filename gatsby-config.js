f359733b59d1f3768732b6f8406ffb38f5ca3f7b
Mark Degani <mark.degani@gmail.com>
2018-01-14 12:17:44 -0500

    minor improvements

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,6 +1,6 @@
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Default Starter`,
+    title: `Mark Degani`,
   },
   plugins: [`gatsby-plugin-react-helmet`],
   pathPrefix: `/`,
--- src/pages/page-2.js
+++ src/pages/page-2.js
@@ -3,8 +3,18 @@ import Link from 'gatsby-link'
 
 const SecondPage = () => (
   <div>
-    <h1>Hi from the second page</h1>
-    <p>Welcome to page 2</p>
+    <h1>About me</h1>
+    <p>I am a former Human Resources spreadsheet builder.</p>
+    <p>MS Excel led me to SQL Server and that led me to Windows Forms, ASP.NET MVC and AngularJS.</p>
+    <p>A couple years ago, I started developing software using JavaScript fulltime.</p>
+    <ul>
+      <li>
+        <a href="https://www.linkedin.com/in/markdegani/">LinkedIn</a>
+      </li>
+      <li>
+        <a href="https://github.com/mdegani">Github</a>
+      </li>
+    </ul>
     <Link to="/">Go back to the homepage</Link>
   </div>
 )
