b7273b0ac312e3d51c751352b2542e1358f74965
Mark Degani <mark.degani@gmail.com>
2018-01-19 07:01:40 -0500

    move stuff outo of pages folder

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,6 +1,6 @@
 import React from "react";
 // import Link from 'gatsby-link'
-import RetroHome from "./retroHome";
+import RetroHome from "../components/retroHome";
 
 const IndexPage = () => (
   <RetroHome />
